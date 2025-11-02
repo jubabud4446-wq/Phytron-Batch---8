401905a4d4f3ecc890a337cb37e5247e2efe4722
jubabud4446-wq <jubabud4446@gmail.com>
2025-11-03 00:42:16 +0600

    Add files via upload

---
+++ phitron/Module 3/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int taka;
+    scanf("%d", &taka);
+    if (taka > 1000) 
+    {
+        printf("I will buy Punjabi\n");
+        taka -= 1000;
+        if (taka >= 500)
+        {
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");     
+        }
+        
+    } 
+    else 
+    {
+        printf("Bad luck!\n");
+    }
+    return 0;
+}
